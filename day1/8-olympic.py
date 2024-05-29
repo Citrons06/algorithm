1311f583e5363d3cf4faf4830f182ef9a4e96ff4
KIM A YEON <yeolee1212@gmail.com>
2024-05-29 22:29:43 +0900

    olympic complete

--- day1/8-olympic.py
+++ day1/8-olympic.py
@@ -5,13 +5,16 @@ medals = [list(map(int, input().split())) for _ in range(n)] # 국가별 메달 
 medals.sort(key=lambda x: (x[1], x[2], x[3]), reverse=True) # 금은동 메달을 튜플로 반환 후 내림차순 정렬
 
 rank = 1
-for i in range(n):
-    # 국가의 번호가 m과 같으면 for문 종료
-    if medals[i][0] == m:
-        break
-    # 현재 국가의 금은동 메달 수와 이전 국가([i-1])의 메달 수가 다른지 확인
-    elif i > 0 and (medals[i][1], medals[i][2], medals[i][3]) != (medals[i-1][1], medals[i-1][2], medals[i-1][3]):
-        # 다를 경우 rank 업데이트
-        rank += 1
+target_rank = 0
 
-print(rank)
+for i in range(n):
+    # 이전 국가와 메달 수가 다른 경우 rank update
+    if i > 0 and (medals[i][1], medals[i][2], medals[i][3]) != (medals[i-1][1], medals[i-1][2], medals[i-1][3]):
+        rank = i + 1
+
+    # 등수를 알고 싶은 국가 찾기
+    if medals[i][0] == m:
+        target_rank = rank
+        break
+
+print(target_rank)
