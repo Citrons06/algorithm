cb5db1c91e69f8cca903ef8bf5df556820318047
KIM A YEON <125535240+Citrons06@users.noreply.github.com>
2024-06-03 19:10:36 +0900

    [Silver III] Title: 숫자 정사각형, Time: 68 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
@@ -5,7 +5,6 @@ ans = []
 for row in range(n):
     for col in range(m):
 
-        cnt = 0
         for i in range(min(m, n)):
             if row + i < n and col + i < m and square[row][col] == square[row + i][col] == square[row][col + i] == square[row + i][col + i]:
                 cnt = i
